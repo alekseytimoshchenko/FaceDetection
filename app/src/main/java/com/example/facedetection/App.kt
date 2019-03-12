cf4ddf3de65f311e6d8b62f58c09d44787c5c465
Aleksey <alekseytimoshchenko@gmail.com>
2019-03-12 11:31:10 +0200

    added configuration libs to project

--- app/src/main/java/com/example/facedetection/App.kt
+++ app/src/main/java/com/example/facedetection/App.kt
@@ -1,11 +1,48 @@
 package com.example.facedetection
 
 import android.app.Application
+import com.facebook.stetho.Stetho
+import com.squareup.leakcanary.LeakCanary
+import timber.log.Timber
 
 class App : Application() {
+    companion object
+    {
+        lateinit var instance: App
+            private set
+    }
+
     override fun onCreate() {
         super.onCreate()
 
 //        startKoin(this, )
+
+        instance = this
+        initStetho()
+        initTimberWithCrashlitics()
+        initLeakCanary()
+    }
+
+    private fun initTimberWithCrashlitics()
+    {
+        Timber.plant(Timber.DebugTree())
+    }
+
+    private fun initStetho()
+    {
+        Stetho.initializeWithDefaults(this)
+    }
+
+    private fun initLeakCanary()
+    {
+        if(LeakCanary.isInAnalyzerProcess(this))
+        {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+
+        LeakCanary.install(this)
+        // Normal app init code...
     }
 }
